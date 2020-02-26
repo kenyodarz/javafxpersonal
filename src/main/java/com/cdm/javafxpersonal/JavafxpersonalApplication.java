a223714b4eeae2d31b6a8e56132b2ac756ca1b2b
Kenyodarz <minajorge@hotmail.com>
2020-02-26 14:30:11 -0500

    Initial commit

---
+++ src/main/java/com/cdm/javafxpersonal/JavafxpersonalApplication.java
@@ -0,0 +1,13 @@
+package com.cdm.javafxpersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafxpersonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavafxpersonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdm/javafxpersonal/JavafxpersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdm.javafxpersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxpersonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
